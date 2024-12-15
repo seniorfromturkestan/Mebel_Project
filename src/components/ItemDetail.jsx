a2cebd6805c723efa221da97082c3abc32c3450c
seniorfromturkestan <yerassyltay2004@mail.ru>
2024-12-15 22:12:19 +0500

    added item details

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -10,7 +10,7 @@ const ItemDetail = ({ items, addToCart }) => {
             <div className="item-detail mt-32">
                  <img src={item.img} alt={item.title} className="w-full h-[400px] object-cover object-center" />
                  <h1 className="text-4xl font-semibold py-4">{item.title}</h1>
-                 <p className="text-xl text-gray-600">{item.description}</p>
+                 <p className="text-lg text-gray-600">{item.description}</p>
                  <p className="text-2xl text-green-600 font-medium">{item.price}₸</p>
                  <div
                       className="border border-gray-600 rounded-lg mt-7 w-48 text-gray-600 text-lg px-4 py-2 cursor-pointer hover:bg-gray-600 hover:text-white transition duration-300"
